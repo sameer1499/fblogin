cb3dfdddf7a5d03ec8335ee8799ae6e14288b1bd
sameer <sameer.sajeev@gmail.com>
2022-01-06 15:54:51 +0530

    Updates

---
+++ precache-manifest.5ab949b6ec859e114a6438280f33002a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f76218ff2a08a692a67489c72ef01d2",
+    "url": "/fblogin/index.html"
+  },
+  {
+    "revision": "53792ef460917acd207f",
+    "url": "/fblogin/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "c03d73acf4bfdb29b904",
+    "url": "/fblogin/static/js/2.1b654696.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/fblogin/static/js/2.1b654696.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53792ef460917acd207f",
+    "url": "/fblogin/static/js/main.3cac2bf3.chunk.js"
+  },
+  {
+    "revision": "85422567870b42ddc6d0",
+    "url": "/fblogin/static/js/runtime-main.4483454e.js"
+  }
+]);
